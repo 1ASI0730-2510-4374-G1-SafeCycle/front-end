7a473b0812c97111598414856e6a83eb75cef53e
yigabriel <gabrielsacc7@gmail.com>
2025-05-09 23:42:08 -0500

    feat: add header and routes

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import {createRouter, createWebHistory} from "vue-router";
+import entryScreenComponent from "@/Authentication/pages/entry-screen.component.vue";
+import signUpStudentComponent from "@/Authentication/pages/sign-up-student.component.vue";
+import signUpTouristComponent from "@/Authentication/pages/sign-up-tourist.component.vue";
+import signInComponent from "@/Authentication/pages/signIn.component.vue";
+import notFoundPageComponent from "@/public/components/notFoundPage.component.vue";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/signup', name: 'signup-page-entry', component: entryScreenComponent },
+        { path: '/signup/student', name: 'signup-page-student', component: signUpStudentComponent },
+        { path: '/signup/tourist', name: 'signup-page-tourist', component: signUpTouristComponent },
+        { path: '/signIn', name: 'signIn', component: signInComponent },
+        { path: '/landing', name: 'landing', beforeEnter() {
+                window.location.href = 'https://1asi0730-2510-4374-g1-safecycle.github.io/landing_page/';
+            } },
+        {path: '/:pathMatch(.*)', name: '404 - nor found', component: notFoundPageComponent}
+    ]
+});
+
+export default router;
--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -4,6 +4,12 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const http = axios.create({
     baseURL: API_BASE_URL,
+    headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+
+    //CORS [Cross-Origin Resource Sharing](trying to access different server)
+    //frontend in localhost:3000 and backend in localhost:5000 [different might cause CORS privacy error]
+    //accept all solicitudes without restrictions
+
 });
 
 export default http;
