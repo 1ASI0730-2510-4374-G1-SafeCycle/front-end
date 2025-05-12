1b0587d4380fbe8a2659c0c6502e1f6746ff410c
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-12 17:02:14 -0500

    chore : create touring-list.component.vue

---
+++ src/Touring/components/touring-list.component.vue
@@ -0,0 +1,21 @@
+<script>
+import {TouringEntity} from "@/Touring/model/touringEntity.js";
+
+export default {
+  name: "touring-list",
+  props:{
+    tourList:{
+      type:Array[TouringEntity],
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-card v-for="tour in tourList" :tour=tour></pv-card>
+</template>
+
+<style scoped>
+
+</style>
