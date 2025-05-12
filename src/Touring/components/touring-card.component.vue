6c239ea8c351e7eeaa0a009717cf4b437918fe74
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-11 20:52:27 -0500

    chore : testing pv-card for touringcomponent

--- src/Touring/components/touring-card.component.vue
+++ src/Touring/components/touring-card.component.vue
@@ -1,11 +1,34 @@
 <script>
+
+import {TouringEntity} from "@/Touring/model/touringEntity.js";
+
 export default {
-  name: "touring-card"
+  name: "touring-card",
+  props: {
+    tour : TouringEntity
+  }
+
 }
 </script>
 
 <template>
-
+<pv-card style="width: 25rem; overflow: hidden">
+  <template #header>
+    <img alt="user header" :src=tour.img
+         style="width: 100%; height: 200px; object-fit: cover;"/>
+  </template>
+  <template #title>{{tour.name}}</template>
+  <template #content>
+    <p class="m-0">
+      {{tour.timeDuration}}
+    </p>
+  </template>
+  <template #footer>
+    <div class="flex gap-4 mt-1">
+      <pv-button label="Go" class="w-full" />
+    </div>
+  </template>
+</pv-card>
 </template>
 
 <style scoped>
