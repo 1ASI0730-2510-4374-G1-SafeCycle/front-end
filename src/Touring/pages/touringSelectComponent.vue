aec4f84d3d1b1b8749b9b907fe5dcd18c04f3488
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-12 21:38:56 -0500

    fix : add promises on methods about API

--- src/Touring/pages/touringSelectComponent.vue
+++ src/Touring/pages/touringSelectComponent.vue
@@ -3,6 +3,7 @@ import {TouringEntity} from "@/Touring/model/touringEntity.js";
 import HeaderContent from "@/public/components/header-content.component.vue";
 import {TourApiService} from "@/Touring/services/tour-api.service.js";
 import TouringList from "@/Touring/components/touring-list.component.vue";
+import {TouringAssembler} from "@/Touring/services/touringAssembler.js";
 
 export default {
   name: "touringSelectComponent",
@@ -13,14 +14,21 @@ export default {
     }
   },
   created(){
-    this.tourData = this.getTourData();
-    console.log(this.tourData);
+    this.getTourData().then(tourData=>{
+      this.tourData = tourData;
+    })
   }
   ,
   methods:{
-    getTourData(){
-      const tourApiService = new TourApiService();
-      return tourApiService.getAllTours();
+    async getTourData(){
+      try {
+        const tourApiService = new TourApiService();
+        const toursRaw = await tourApiService.getAllTours();
+        return TouringAssembler.ToursFromResponse(toursRaw);
+      }
+      catch(error){
+        console.log(error)
+      }
     }
   }
 }
@@ -28,7 +36,7 @@ export default {
 
 <template>
  <header-content></header-content>
-  <touring-list :tour-list=tourData></touring-list>
+  <touring-list :tourList=tourData></touring-list>
 </template>
 
 <style scoped>
