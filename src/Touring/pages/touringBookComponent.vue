fd0f534ae1a92d0d023420d88b7b71ab37a81a7d
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-12 22:50:29 -0500

    chore : add components on touringBookComponent.vue

--- src/Touring/pages/touringBookComponent.vue
+++ src/Touring/pages/touringBookComponent.vue
@@ -1,11 +1,16 @@
 <script>
+import TouringForm from "@/Touring/components/touring-form.component.vue";
+import HeaderContent from "@/public/components/header-content.component.vue";
+
 export default {
-  name: "touringBookComponent"
+  name: "touringBookComponent",
+  components: {HeaderContent, TouringForm}
 }
 </script>
 
 <template>
-
+  <header-content></header-content>
+  <touring-form></touring-form>
 </template>
 
 <style scoped>
