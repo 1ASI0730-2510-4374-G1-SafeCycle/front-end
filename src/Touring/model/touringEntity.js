3c176595ba73ce9e081c483de3ad50efe742158d
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-13 13:50:01 -0500

    chore : add attribute id on touringEntity.js

--- src/Touring/model/touringEntity.js
+++ src/Touring/model/touringEntity.js
@@ -1,10 +1,12 @@
 export class TouringEntity {
     constructor(
+        id = 0,
         name = "",
         hour = "",
         img = "",
         price = 0.0,
     ) {
+        this.id = id;
         this.name = name;
         this.hour = hour;
         this.img = img;
