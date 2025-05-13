c2c860122d8c8fb0a656cfe754170fff7e5e438f
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-12 21:37:04 -0500

    fix : change pv-card for touring-card on touring-list.component.vue

--- src/Touring/components/touring-list.component.vue
+++ src/Touring/components/touring-list.component.vue
@@ -1,8 +1,10 @@
 <script>
 import {TouringEntity} from "@/Touring/model/touringEntity.js";
+import TouringCard from "@/Touring/components/touring-card.component.vue";
 
 export default {
   name: "touring-list",
+  components: {TouringCard},
   props:{
     tourList:{
       type:Array[TouringEntity],
@@ -12,10 +14,24 @@ export default {
 }
 </script>
 
-<template>
-  <pv-card v-for="tour in tourList" :tour=tour></pv-card>
+<<template>
+  <div class="card-container">
+    <touring-card
+        v-for="tour in tourList"
+        :key="tour.name"
+        :tour="tour"
+    />
+  </div>
 </template>
 
 <style scoped>
-
+.card-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  min-height: 100vh;
+  padding: 2rem;
+}
 </style>
