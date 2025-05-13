81e918eee60ea7cd6b95cb9c1c257fe724ad4d21
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-12 21:38:05 -0500

    chore : create touringAssembler.js for api response

---
+++ src/Touring/services/touringAssembler.js
@@ -0,0 +1,13 @@
+import {TouringEntity} from "@/Touring/model/touringEntity.js";
+
+export class TouringAssembler {
+    static ToursFromResponse(response){
+        const toursData = response.data;
+        return toursData.map((tour) => this.TourFromResponse(tour));
+
+    }
+    static TourFromResponse(tour){
+        console.log(tour);
+        return new TouringEntity(tour.name, tour.hour, tour.img);
+    }
+}
