7a697917f119485a5555fa49f5eb123f3c951a04
diegoalonzomora <u20231c069@upc.edu.pe>
2025-05-13 21:56:09 -0500

    feat : add and create TourBookEntity

---
+++ src/Touring/model/tourBookEntity.js
@@ -0,0 +1,19 @@
+export class TourBookEntity{
+    constructor(
+        id=0,
+        name="",
+        start_date="",
+        start_hour="",
+        bike_station="",
+        duration="",
+        cost=""
+    ){
+        this.id = id;
+        this.name = name;
+        this.start_date = start_date;
+        this.start_hour = start_hour;
+        this.bike_station = bike_station;
+        this.duration = duration;
+        this.cost = cost;
+    }
+}
